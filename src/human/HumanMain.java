48adb34d23eb9b99885c4dd7510cc936f3dafe88
Rafał Nowak <rafal1478@gmail.com>
2019-11-17 09:21:14 +0100

    name cheanged

--- src/human/HumanMain.java
+++ src/human/HumanMain.java
@@ -2,7 +2,9 @@ package human;
 
 public class HumanMain {
     public static void main(String[] args) {
-        Human adam = new Human ("adam", 12, EyeColor.Brown, HairColor.Pink);
+        Human adam = new Human ("natalia", 12, EyeColor.Brown, HairColor.Pink);
         System.out.println(adam.toString());
+        adam = new Human("adam",15,);
     }
+
 }
--- src/klasap/Newspaper.java
+++ src/klasap/Newspaper.java
@@ -1,17 +1,14 @@
-package klasap;
+//package klasap;
 
-public class Newspaper extends Publication {
-    private String source;
-
-    public Newspaper(String title, String source) {
-        super(title);
-        this.source = source;
-    }
-
-    public String getDetails() {
-
-        return "title=\"" + title + "\"" + "source=\"" + source + "\"";
+//public class Newspaper extends Publication {
+//    private String source;
+//
+//    public Newspaper(String title, String source) {
+//        super(title);
+//        this.source = source;
+//    }
+//
+//    public String getDetails() {
+//
+//        return "title=\"" + title + "\"" + "source=\"" + source + "\"";
         // write your code here
-    }
-
-}
